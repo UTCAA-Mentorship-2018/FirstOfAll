5e1e4e50a2d8fdb55f0918ae2c24bceccdb19942
HuangGuanzhi <35811946+HuangGuanzhi@users.noreply.github.com>
2018-12-08 18:54:53 -0500

    Update Encoding.py

--- iteration1/data_preprocessing/Encoding.py
+++ iteration1/data_preprocessing/Encoding.py
@@ -9,7 +9,10 @@ Created on Sun Nov 25 22:45:11 2018
 import pandas as pd
 
 # Encoding
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+
+# Spliting data
+from sklearn.model_selection import train_test_split
 
 def Encoding_Data(data):
     df = data.copy()
@@ -32,3 +35,19 @@ def Encoding_Data(data):
 
     print('%d columns were label encoded' % labelcount)
     return df
+
+def scale_split(data, target, test_pro1, test_pro2):
+    
+    scaled = {}
+    
+    data_train, data_test, target_train, target_test = train_test_split(data, target, test_size = test_pro1, shuffle = True)
+    data_train, data_val, target_train, target_val = train_test_split(data_train, target_train, test_size = test_pro2, shuffle = True)
+    
+    data_scaled = StandardScaler()
+    data_scaled.fit(data_train)
+    
+    data_train_scaled = data_scaled.transform(data_train)
+    data_test_scaled = data_scaled.transform(data_test)
+    data_val_scaled = data_scaled.transform(data_val)
+    
+    return data_train_scaled, data_test_scaled, data_val_scaled, target_train, target_test, target_val
